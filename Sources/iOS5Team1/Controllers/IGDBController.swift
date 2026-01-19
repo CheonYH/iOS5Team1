9dbf4ffc8f4e0cdfbe82eed9641ced2ed44e97bc
cheon <cun76530285@gmail.com>
2026-01-19 17:46:53 +0900

    fix: IGDBController 라우트 및 응답 처리 수정

--- Sources/iOS5Team1/Controllers/IGDBController.swift
+++ Sources/iOS5Team1/Controllers/IGDBController.swift
@@ -9,32 +9,60 @@ import Vapor
 
 struct IGDBController: RouteCollection {
     func boot(routes: any RoutesBuilder) throws {
-        routes.post("igdb", ":endpoint") { req async throws -> Response in
-            guard let endpoint = req.parameters.get("endpoint") else {
-                throw Abort(.badRequest)
-            }
+        let v4 = routes.grouped("v4")
+        v4.post("multiquery", use: multiquery)
+        v4.post("games", use: gameDetail)
+    }
 
-            let token = try await req.igdb.getAccessToken(client: req.client)
+    // Multiquery
+    func multiquery(req: Request) async throws -> Response {
+        let token = try await req.igdb.getAccessToken(client: req.client)
 
-            let body = req.body.data ?? ByteBuffer()
-            let url = URI(string: "https://api.igdb.com/v4/\(endpoint)")
+        var igdbReq = ClientRequest()
+        igdbReq.method = .POST
+        igdbReq.url = URI(string: "https://api.igdb.com/v4/multiquery")
+        igdbReq.headers.replaceOrAdd(name: .authorization, value: "Bearer \(token)")
+        igdbReq.headers.replaceOrAdd(name: "Client-ID", value: req.igdb.clientId)
+        igdbReq.headers.replaceOrAdd(name: "Content-Type", value: "text/plain")
+        igdbReq.body = req.body.data
 
-            let igdbResponse = try await req.client.post(url) { igdbReq in
-                igdbReq.headers.add(name: "Client-ID", value: req.igdb.clientId)
-                igdbReq.headers.add(name: "Authorization", value: "Bearer \(token)")
-                igdbReq.headers.add(name: "Content-Type", value: "text/plain")
-                igdbReq.body = body
-            }
+        let clientRes = try await req.client.send(igdbReq)
 
-            var res = Response(status: igdbResponse.status)
+        // Server Response 생성
+        var res = Response(status: clientRes.status)
+        res.headers = clientRes.headers
 
-            res.headers = igdbResponse.headers
-            if let buffer = igdbResponse.body {
-                res.body = .init(buffer: buffer)
-            }
-
-            return res
+        if let buffer = clientRes.body {
+            res.body = .init(buffer: buffer)
         }
+
+        return res
+    }
+
+    // Detail query
+    func gameDetail(req: Request) async throws -> Response {
+        let token = try await req.igdb.getAccessToken(client: req.client)
+
+        var igdbReq = ClientRequest()
+        igdbReq.method = .POST
+        igdbReq.url = URI(string: "https://api.igdb.com/v4/games")
+        igdbReq.headers.replaceOrAdd(name: .authorization, value: "Bearer \(token)")
+        igdbReq.headers.replaceOrAdd(name: "Client-ID", value: req.igdb.clientId)
+        igdbReq.headers.replaceOrAdd(name: "Content-Type", value: "text/plain")
+        igdbReq.body = req.body.data
+
+        let clientRes = try await req.client.send(igdbReq)
+
+        // Server Response 생성
+        var res = Response(status: clientRes.status)
+        res.headers = clientRes.headers
+
+        if let buffer = clientRes.body {
+            res.body = .init(buffer: buffer)
+        }
+
+        return res
     }
 }
+
 
